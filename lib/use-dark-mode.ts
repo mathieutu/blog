188b72e322480098bfd6bafb72141ba4fec67eb3
Mathieu TUDISCO <mathieu@tudisco.fr>
2026-04-27 16:28:29 +0200

    🐛 Fix issue with darkmode ssr.

--- lib/use-dark-mode.ts
+++ lib/use-dark-mode.ts
@@ -1,7 +1,15 @@
 import useDarkModeImpl from '@fisch0920/use-dark-mode'
 
+const noopStorage = {
+  getItem: () => null,
+  setItem: () => {}
+} as unknown as WindowLocalStorage
+
 export function useDarkMode() {
-  const darkMode = useDarkModeImpl(false, { classNameDark: 'dark-mode' })
+  const darkMode = useDarkModeImpl(false, {
+    classNameDark: 'dark-mode',
+    storageProvider: typeof window !== 'undefined' ? window.localStorage as unknown as WindowLocalStorage : noopStorage
+  })
 
   return {
     isDarkMode: darkMode.value,
